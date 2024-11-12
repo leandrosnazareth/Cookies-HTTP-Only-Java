7e4c94a793d2bae12bec2a4a8a9eb5d766017bd7
Leandro Nazareth <leandrosnazareth@gmail.com>
2024-11-12 20:29:03 -0300

    first commit

---
+++ src/main/java/br/com/leandrosnazareth/Cookies_HTTP_Only_Java/CookiesHttpOnlyJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrosnazareth.Cookies_HTTP_Only_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiesHttpOnlyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookiesHttpOnlyJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandrosnazareth/Cookies_HTTP_Only_Java/CookiesHttpOnlyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrosnazareth.Cookies_HTTP_Only_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiesHttpOnlyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
